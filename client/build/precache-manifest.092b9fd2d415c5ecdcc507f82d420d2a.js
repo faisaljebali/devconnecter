23cccbe574870542e47c4c58807a63bb48522513
Faisal Jebali <faisal.jebali@satoripop.com>
2020-04-26 17:27:53 +0100

    first commit 2

---
+++ client/build/precache-manifest.092b9fd2d415c5ecdcc507f82d420d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6941c4e5837f64b84d94f6aee55b58ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31c9c449a6e6f7ed6af3",
+    "url": "/static/css/main.d8116c63.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "31c9c449a6e6f7ed6af3",
+    "url": "/static/js/main.df049563.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
